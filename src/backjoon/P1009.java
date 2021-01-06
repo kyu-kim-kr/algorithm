2815c41e75bd99fb82f4664cda653e0aa67e31c6
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-01-06 18:54:55 +1300

    P1009: BigInteger 메모리 초과로 인해 로직 변경

--- src/backjoon/P1009.java
+++ src/backjoon/P1009.java
@@ -1,7 +1,7 @@
 package backjoon;
 
 import java.io.*;
-import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class P1009 {
@@ -12,18 +12,29 @@ public class P1009 {
         int T = Integer.parseInt(br.readLine());
 
         StringTokenizer st;
-        BigInteger bigNum;
+
+        ArrayList<Integer> remain = new ArrayList<>();
 
         for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            bigNum = (BigInteger.valueOf(a).pow(b)).remainder(BigInteger.TEN);
-            if (bigNum == BigInteger.ZERO) {
-                bigNum = BigInteger.TEN;
+            remain.add(a);
+            int A = a;
+            while (true) {
+                remain.add((int) Math.pow(a, A) % 10);
+                //TODO : 10의 배수 처리
+                a = (a * A)%10;
+                if (a == A) {
+                    break;
+                }
             }
-
-            bw.write(bigNum + "\n");
+            int indexOfRemain = b % remain.size() - 1;
+            if (indexOfRemain == -1) {
+                indexOfRemain = remain.size() - 1;
+            }
+            bw.write(remain.get(indexOfRemain) + "\n");
+            remain.clear();
         }
         bw.flush();
         bw.close();
