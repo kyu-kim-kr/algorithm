187948a395d3466d22d5ad5af91b5444c06e9862
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-01-13 15:22:37 +1300

    leetcode: two-sum 수정 완벽

--- src/leetcode/TwoSum.java
+++ src/leetcode/TwoSum.java
@@ -1,7 +1,9 @@
 package leetcode;
 
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
+import java.util.Arrays;
+
+class TwoSum {
+    public static int[] twoSum ( int[] nums, int target){
         /**
          * 0 + 1 , 0 + 2 , 0 + 3
          * 1 + 2 , 1 + 3
@@ -9,21 +11,29 @@ class Solution {
          */
         int[] answer = new int[2];
 
-        int count = nums.length;
         for (int i = 0; i < nums.length; i++) {
-            count--;
-            for (int j = 0; j < count; j++) {
-                if (i <= j) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i >= j) {
                     continue;
                 }
                 if (target == nums[i] + nums[j]) {
-                 answer[0] = i;
-                 answer[1] = j;
-                 return answer;
+                    answer[0] = i;
+                    answer[1] = j;
+                    return answer;
                 }
 
             }
         }
         return answer;
     }
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{3, 2, 4, 6}, 6)));
+        /**
+         * 문제1
+         * new로 바로 인트배열만들었는데빨간줄
+         * 문제2
+         * 저렇게안하고 변수에 배열담아서 출력했는데 메모리 주소가 나옴
+         */
+    }
 }
+
