9a12d1ad77d8e6041d3fdc90a4d88effcbacd030
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-01-21 07:12:15 +1300

    아아아아아 왜 안되는건데?????

--- src/programmers/p64061/ClawClaneGame.java
+++ src/programmers/p64061/ClawClaneGame.java
@@ -2,13 +2,12 @@ package programmers.p64061;
 
 public class ClawClaneGame {
     public static void main(String[] args) {
-        int[][] board = {{0, 0, 1, 0, 0, 0}, //board[0]
-                {0, 2, 0, 3, 0, 5}, // board[1]
-                {0, 0, 0, 4, 0, 5},
-                {0, 4, 0, 3, 0, 0},
-                {1, 0, 5, 0, 6, 0},
-                {0, 0, 0, 7, 0, 0}};
-        int[] moves = {1, 5, 3, 5, 1, 2, 1, 4, 5, 6, 1, 3, 5, 6};
+        int[][] board = {{0, 0, 0, 0, 0}, //board[0]
+                {0, 0, 1, 0, 3}, // board[1]
+                {0, 2, 5, 0, 1},
+                {4, 2, 4, 4, 2},
+                {3, 5, 1, 3, 1}};
+        int[] moves = {1, 5, 3, 5, 1, 2, 1, 4};
 
         Solution s = new Solution();
         s.solution(board, moves);
--- src/programmers/p64061/Solution.java
+++ src/programmers/p64061/Solution.java
@@ -5,12 +5,13 @@ import java.util.ArrayList;
 public class Solution {
 
     ArrayList<Integer> MovedDolls = new ArrayList<>();
+    int count = 0;
 
     public void solution(int[][] board, int[] moves) {
         moveDolls(board, moves);
-        System.out.println(checkDuplicates());
+        System.out.println(count);
     }
-ㅇ
+
     private void moveDolls(int[][] board, int[] moves) {
 
         for (int i = 0; i < moves.length; i++) {
@@ -20,29 +21,30 @@ public class Solution {
                 if (board[moves[i] - 1][lastIndex] != 0) {
                     int caughtDoll = board[moves[i] - 1][lastIndex];
                     MovedDolls.add(caughtDoll);
-                    board[moves[i] - 1][lastIndex] = 0; // 옮긴걸 0으로바꾸면 다음엔 안집겠네
+                    board[moves[i] - 1][lastIndex] = 0; //옮긴걸 0으로바꾸면 다음엔 안집겠네
+
+                    checkDuplicates(MovedDolls);
+
+
                     break; //옮겨다 집어넣으면 다음 move.
                 }
             }
 
         }
-        System.out.println(MovedDolls);
     }
 
-    private int checkDuplicates() {
-        int count = 0;
+    private void checkDuplicates(ArrayList<Integer> MovedDolls) {
 
-        for (int i = 0; i < MovedDolls.size(); i++ ) {
-            if (i+1 == MovedDolls.size()) {
+        for (int i = 0; i < MovedDolls.size(); i++) {
+            if (i + 1 == MovedDolls.size()) {
                 break;
             }
-            if (MovedDolls.get(i) == MovedDolls.get(i+1) ) {
+            if (MovedDolls.get(i) == MovedDolls.get(i + 1)) {
                 count += 2;
-                i++;
+                MovedDolls.remove(i); // 여기서 length가 2가되고
+                MovedDolls.remove(i); // 여기서 또 지우려니까  length가 2인데 어떻게지우냐 이말이네?
+                i--; //remove하면 사이즈가 줄어드니까 다음 비교대상의 인덱스가 줄어들게된다 그래서 i--;
             }
-
         }
-
-        return count;
     }
 }
