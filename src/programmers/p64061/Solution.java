2ba99ec2d4f642bf3e47fbb780b21102683992c0
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-01-21 11:54:24 +1300

    파라미터 이름변경

--- src/programmers/p64061/Solution.java
+++ src/programmers/p64061/Solution.java
@@ -2,40 +2,46 @@ package programmers.p64061;
 
 import java.util.ArrayList;
 
-class Solution {
-    int answer = 0;
+public class Solution {
+
+    ArrayList<Integer> MovedDolls = new ArrayList<>();
+    int count = 0;
 
     public int solution(int[][] board, int[] moves) {
+        moveDolls(board, moves);
+        return count;
+    }
 
-        ArrayList<Integer> basket = new ArrayList<>();
+    private void moveDolls(int[][] board, int[] moves) {
 
         for (int i = 0; i < moves.length; i++) {
-            int locationOfCrane = moves[i] - 1;
-            for (int j = 0; j < board.length; j++) { //board.length 만큼 0이 안나오는곳까지 try.
-                if (board[j][locationOfCrane] != 0) {
-                    basket.add(board[j][locationOfCrane]);
-                    board[j][locationOfCrane] = 0;
+            for (int j = 0; j < board.length; j++) {
 
-                    checkDuplicates(basket);
+                int lastIndex = board.length-(j+1);
+                if (board[moves[i] - 1][lastIndex] != 0) {
+                    int caughtDoll = board[moves[i] - 1][lastIndex];
+                    MovedDolls.add(caughtDoll);
+                    board[moves[i] - 1][lastIndex] = 0;
+
+                    checkDuplicates(MovedDolls);
 
                     break;
                 }
             }
-        }
 
-        return answer;
+        }
     }
 
-    private void checkDuplicates(ArrayList<Integer> MovedDolls) {
+    private void checkDuplicates(ArrayList<Integer> basket) {
 
-        for (int i = 0; i < MovedDolls.size(); i++) {
-            if (i + 1 == MovedDolls.size()) {
+        for (int i = 0; i < basket.size(); i++) {
+            if (i + 1 == basket.size()) {
                 break;
             }
-            if (MovedDolls.get(i) == MovedDolls.get(i + 1)) {
-                answer += 2;
-                MovedDolls.remove(i);
-                MovedDolls.remove(i);
+            if (basket.get(i) == basket.get(i + 1)) {
+                count += 2;
+                basket.remove(i);
+                basket.remove(i);
                 i--; //remove하면 사이즈가 줄어드니까 다음 비교대상의 인덱스가 줄어들게된다 그래서 i--;
             }
         }
