4344271eedcc3eabb3115e167c09dfe44c6fdddf
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-01-21 11:45:00 +1300

    오 드뎌 성공!!!!!!!!!ㅋㅋ

--- src/programmers/p64061/Solution.java
+++ src/programmers/p64061/Solution.java
@@ -2,35 +2,28 @@ package programmers.p64061;
 
 import java.util.ArrayList;
 
-public class Solution {
+class Solution {
+    int answer = 0;
 
-    ArrayList<Integer> MovedDolls = new ArrayList<>();
-    int count = 0;
+    public int solution(int[][] board, int[] moves) {
 
-    public void solution(int[][] board, int[] moves) {
-        moveDolls(board, moves);
-        System.out.println(count);
-    }
-
-    private void moveDolls(int[][] board, int[] moves) {
+        ArrayList<Integer> basket = new ArrayList<>();
 
         for (int i = 0; i < moves.length; i++) {
-            for (int j = 0; j < board.length; j++) {
+            int locationOfCrane = moves[i] - 1;
+            for (int j = 0; j < board.length; j++) { //board.length 만큼 0이 안나오는곳까지 try.
+                if (board[j][locationOfCrane] != 0) {
+                    basket.add(board[j][locationOfCrane]);
+                    board[j][locationOfCrane] = 0;
 
-                int lastIndex = board.length-(j+1); //temp의 마지막인덱스
-                if (board[moves[i] - 1][lastIndex] != 0) {
-                    int caughtDoll = board[moves[i] - 1][lastIndex];
-                    MovedDolls.add(caughtDoll);
-                    board[moves[i] - 1][lastIndex] = 0; //옮긴걸 0으로바꾸면 다음엔 안집겠네
+                    checkDuplicates(basket);
 
-                    checkDuplicates(MovedDolls);
-
-
-                    break; //옮겨다 집어넣으면 다음 move.
+                    break;
                 }
             }
-
         }
+
+        return answer;
     }
 
     private void checkDuplicates(ArrayList<Integer> MovedDolls) {
@@ -40,9 +33,9 @@ public class Solution {
                 break;
             }
             if (MovedDolls.get(i) == MovedDolls.get(i + 1)) {
-                count += 2;
-                MovedDolls.remove(i); // 여기서 length가 2가되고
-                MovedDolls.remove(i); // 여기서 또 지우려니까  length가 2인데 어떻게지우냐 이말이네?
+                answer += 2;
+                MovedDolls.remove(i);
+                MovedDolls.remove(i);
                 i--; //remove하면 사이즈가 줄어드니까 다음 비교대상의 인덱스가 줄어들게된다 그래서 i--;
             }
         }
